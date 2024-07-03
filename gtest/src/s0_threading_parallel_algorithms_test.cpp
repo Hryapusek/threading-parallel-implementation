6afa92bd5541c932fddbdd63f8f6e634350e7d7f
Abraamyan Alexander <samvelcc25@outlook.com>
2024-07-03 17:14:22 +0300

    reduce algorithm with init value created
    Also reduce algorithm with 2 parameters
    does not create int value from zero value

--- gtest/src/s0_threading_parallel_algorithms_test.cpp
+++ gtest/src/s0_threading_parallel_algorithms_test.cpp
@@ -6,11 +6,21 @@
 
 #include "s0_parallel_algorithms_threading.hpp"
 
-TEST(reduceParallelAlgo, VectorOf500Elements)
+TEST(reduceParallelAlgorithm, VectorOf500Elements)
 {
     auto range = std::ranges::views::iota(0, 500);
     std::vector<int> arr(range.begin(), range.end());
     s0m4b0dY::Threading threading;
     auto result = threading.reduce(arr.begin(), arr.end());
     ASSERT_EQ(result, std::reduce(arr.begin(), arr.end()));
+}
+
+TEST(reduceParallelAlgorithm, VectorOf500ElementsInitValuePassed)
+{
+    auto range = std::ranges::views::iota(0, 500);
+    std::vector<int> arr(range.begin(), range.end());
+    s0m4b0dY::Threading threading;
+    int initValue = 200;
+    auto result = threading.reduce(arr.begin(), arr.end(), initValue);
+    ASSERT_EQ(result, initValue + std::reduce(arr.begin(), arr.end()));
 }
--- include/public/s0_parallel_algorithms_threading.hpp
+++ include/public/s0_parallel_algorithms_threading.hpp
@@ -13,36 +13,89 @@ namespace s0m4b0dY
 {
   class Threading
   {
+    template < class T >
+    using IteratorValueType = _helpers::IteratorValueType<T>;
   public:
     template <_helpers::AddableIterator Iterator_t>
-    _helpers::IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end);
+    IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end);
+
+    template <_helpers::AddableIterator Iterator_t>
+    IteratorValueType<Iterator_t>::value_type reduce(Iterator_t begin, Iterator_t end, IteratorValueType<Iterator_t>::value_type initValue);
   };
 
   template <_helpers::AddableIterator Iterator_t>
   inline _helpers::IteratorValueType<Iterator_t>::value_type Threading::reduce(Iterator_t begin, Iterator_t end)
   {
     using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
-    const value_type init_value = 0;
     std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, std::thread::hardware_concurrency());
-    std::vector<std::future<value_type>> results;
+    std::vector<std::future<std::optional<value_type>>> results;
     for (auto i = 0; i < ranges.size(); ++i)
     {
       results.push_back(std::async(
-        [&range=ranges[i],init_value]()
+        [&range=ranges[i]]() -> std::optional<value_type>
         {
-          value_type result = init_value;
-          for (auto it = range.first; it != range.second; it++)
+          auto it = range.first;
+          if (it == range.second)
+          {
+            return std::nullopt;
+          }
+          value_type result = *it++;
+          for (; it != range.second; it++)
           {
             result += *it;
           }
           return result;
-        })
-      );
+        }
+      ));
     }
-    auto result = init_value;
-    for (std::future<value_type> &future : results)
+    std::optional<value_type> result;
+    for (std::future<std::optional<value_type>> &future : results)
     {
-      result += future.get();
+      std::optional<value_type> value = future.get();
+      if (value.has_value())
+      {
+        if (result.has_value())
+          *result += std::move(value).value();
+        else
+          result = std::move(value).value();
+      }
+    }
+    if (not result.has_value())
+      throw std::logic_error("Zero values in reduce found");
+    return *result;
+  }
+
+  template <_helpers::AddableIterator Iterator_t>
+  inline _helpers::IteratorValueType<Iterator_t>::value_type Threading::reduce(Iterator_t begin, Iterator_t end, IteratorValueType<Iterator_t>::value_type initValue)
+  {
+    using value_type = _helpers::IteratorValueType<Iterator_t>::value_type;
+    std::vector<std::pair<Iterator_t, Iterator_t>> ranges = generateRanges(begin, end, std::thread::hardware_concurrency());
+    std::vector<std::future<std::optional<value_type>>> results;
+    for (auto i = 0; i < ranges.size(); ++i)
+    {
+      results.push_back(std::async(
+        [&range=ranges[i]]() -> std::optional<value_type>
+        {
+          auto it = range.first;
+          if (it == range.second)
+          {
+            return std::nullopt;
+          }
+          value_type result = *it++;
+          for (; it != range.second; it++)
+          {
+            result += *it;
+          }
+          return result;
+        }
+      ));
+    }
+    auto result = initValue;
+    for (std::future<std::optional<value_type>> &future : results)
+    {
+      std::optional<value_type> value = future.get();
+      if(value.has_value())
+        result += std::move(value).value();
     }
     return result;
   }
